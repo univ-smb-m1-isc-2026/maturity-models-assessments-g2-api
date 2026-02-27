5280efc75b3b9dd1682c680de0bc39d47bf9268b
Vineastevia <156806211+Vineastevia@users.noreply.github.com>
2026-02-27 11:42:08 +0100

    add CrossOrigin

--- src/main/java/fr/univ_smb/info803/maturitymodelsassessmentsg2api/HelloController.java
+++ src/main/java/fr/univ_smb/info803/maturitymodelsassessmentsg2api/HelloController.java
@@ -1,12 +1,14 @@
 package fr.univ_smb.info803.maturitymodelsassessmentsg2api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }
