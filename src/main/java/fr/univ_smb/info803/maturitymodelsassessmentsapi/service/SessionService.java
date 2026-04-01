85d066ff356fee18f4be180f2d093b3142fc8f4d
Vineastevia <156806211+Vineastevia@users.noreply.github.com>
2026-04-01 19:23:52 +0200

    fix: sessionController delete session route

--- src/main/java/fr/univ_smb/info803/maturitymodelsassessmentsapi/service/SessionService.java
+++ src/main/java/fr/univ_smb/info803/maturitymodelsassessmentsapi/service/SessionService.java
@@ -3,9 +3,11 @@ package fr.univ_smb.info803.maturitymodelsassessmentsapi.service;
 import fr.univ_smb.info803.maturitymodelsassessmentsapi.model.SessionStatus;
 import fr.univ_smb.info803.maturitymodelsassessmentsapi.model.Session;
 import fr.univ_smb.info803.maturitymodelsassessmentsapi.repository.SessionRepository;
+import fr.univ_smb.info803.maturitymodelsassessmentsapi.repository.SessionResultRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +18,7 @@ import java.util.Optional;
 public class SessionService {
 
     private final SessionRepository sessionRepository;
+    private final SessionResultRepository sessionResultRepository;
 
     public List<Session> getByTeamId(Long teamId){ return sessionRepository.findByTeamId(teamId); }
 
@@ -27,8 +30,12 @@ public class SessionService {
 
     public List<Session> getSessions(){ return sessionRepository.findAll(); }
 
-    public void deleteSession(final Long id){ sessionRepository.deleteById(id); }
-
     public Session saveSession(Session session){ return sessionRepository.save(session); }
+
+    @Transactional
+    public void deleteSession(final Long id){
+        sessionResultRepository.deleteBySessionId(id);
+        sessionRepository.deleteById(id);
+    }
 
 }
